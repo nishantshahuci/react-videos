a99c42d78d27f4d3fb173a24a76fa960b0691e64
Nishant Shah <nishantshahuci@gmail.com>
2019-02-17 15:21:45 -0500

    Show video details when user clicks on a video

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 import youtube from '../api/youtube';
 
 class App extends Component {
@@ -30,6 +31,7 @@ class App extends Component {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
+                <VideoDetail video={this.state.selectedVideo} />
                 <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
             </div>
         );
